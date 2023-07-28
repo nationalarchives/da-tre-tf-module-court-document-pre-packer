c18dd657d442e97ed274f39e0f6db4ba29d82566
Mark Kingsbury <mark@kingsburydata.com>
2023-07-28 17:14:51 +0100

    DTE-844: take out permits to call success from here

--- iam.tf
+++ iam.tf
@@ -16,24 +16,7 @@ resource "aws_iam_role" "tre_court_document_pre_packer_role" {
   permissions_boundary = var.tre_permission_boundary_arn
 }
 
-resource "aws_iam_role_policy_attachment" "tre_success_lambda_logs" {
+resource "aws_iam_role_policy_attachment" "tre_court_document_pre_packer_lambda_logs" {
   role       = aws_iam_role.tre_court_document_pre_packer_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSOpsWorksCloudWatchLogs"
-}
-
-data "aws_iam_policy_document" "pre_packer_lambda_invoke_policy_data" {
-  statement {
-    sid     = "InvokeLambdaPolicy"
-    effect  = "Allow"
-    actions = ["lambda:InvokeFunction"]
-    resources = [
-      var.success_handler_lambda_arn
-    ]
-  }
-}
-
-resource "aws_iam_policy" "pre_packer_lambda_invoke_policy" {
-  name        = "${var.env}-${var.prefix}-pre-packer-lambda-invoke"
-  description = "The policy for pre packer lambda to invoke success lambda"
-  policy      = data.aws_iam_policy_document.pre_packer_lambda_invoke_policy_data.json
 }
