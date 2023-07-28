fbd2a15cd306fc881e5feb62cf28f79ef6c40e73
Mark Kingsbury <mark@kingsburydata.com>
2023-07-28 16:44:48 +0100

    DTE-844: output lambda arn not role

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "tre_court_document_pre_packer_lambda_arn" {
-  value       = aws_iam_role.tre_court_document_pre_packer_role.arn
+  value       = aws_lambda_function.tre_court_document_pre_packer.arn
   description = "TRE Court Document Pre Packer Lambda Role ARN"
 }
